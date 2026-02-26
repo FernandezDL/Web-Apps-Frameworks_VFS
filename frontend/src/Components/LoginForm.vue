93e59299b531d12ef38b8df40c875a1dcca70ebc
Diana Fernández <dianaluciafv@gmail.com>
2026-02-26 12:19:40 -0800

    Add: Login
    Adding login page and login component

---
+++ frontend/src/Components/LoginForm.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="login-form">
+        <input type="text" placeholder="Username"/>
+        <input type="password" placeholder="Password"/>
+
+        <button class="LoginBttn">Login</button>
+    </div> 
+</template>
+
+<style scoped>
+    .LoginBttn {
+        width: 300px;
+
+        margin-top: 50px;
+        padding: 15px;
+        font-size: 20px;
+        cursor: pointer;
+
+        background-color: #FF8FAB;
+
+        border-radius: 10px;
+        border: none;
+    }
+
+    .LoginBttn:hover {
+        background-color: #FB6F92;
+
+        color: white;
+    }
+
+    .login-form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 10px;
+
+        margin-top: 20px;
+    }
+
+    input {
+        width: 300px;
+        padding: 10px;
+        font-size: 16px;
+
+        border-radius: 5px;
+    }
+
+</style>
---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import LoginForm from "@/Components/LoginForm.vue";
+</script>
+
+<template>
+    <div class="login-page">
+        <h1>Welcome!</h1>
+        
+        <LoginForm/>
+    </div>
+</template>
+
+<style scoped>
+    .login-page {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+    }
+</style>
